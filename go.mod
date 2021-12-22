e9111f95ea8c54e1123b3e71bf7b2241354dab14
Brian Stewart <brian.stewart@jamf.com>
2021-12-22 13:39:25 -0600

    Initial commit of Go project and functional app

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/thecodesmith/filesystem-watcher
+
+go 1.17
+
+require github.com/radovskyb/watcher v1.0.7
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/radovskyb/watcher"
+)
+
+func main() {
+	w := watcher.New()
+
+	// Only notify rename and move events.
+	// w.FilterOps(watcher.Rename, watcher.Move)
+
+	// Only files that match the regular expression during file listings
+	// will be watched.
+	// r := regexp.MustCompile("^abc$")
+	// w.AddFilterHook(watcher.RegexFilterHook(r, false))
+
+	go func() {
+		for {
+			select {
+			case event := <-w.Event:
+				printEvent(event)
+			case err := <-w.Error:
+				log.Fatalln(err)
+			case <-w.Closed:
+				return
+			}
+		}
+	}()
+
+	// Watch this folder for changes.
+	if err := w.AddRecursive("."); err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println("Watching for changes")
+
+	// Start the watching process - it'll check for changes every 100ms.
+	if err := w.Start(time.Millisecond * 100); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func printEvent(event watcher.Event) {
+    fmt.Printf("%v  %v\n", event.Op, event.Path) // Print the event's info.
+}
